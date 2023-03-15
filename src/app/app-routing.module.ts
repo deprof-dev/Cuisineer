fdbb75029fdc6b8a40e23418307f82f48b53f5eb
AbdulSalam AbdulRahman <abdulrahmanabdulsalam93@gmail.com>
2023-03-15 23:04:27 +0100

    chore: completed initial setup and tabs page lookout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,43 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'edit-recipe',
+    loadChildren: () => import('./edit-recipe/edit-recipe.module').then(m => m.EditRecipePageModule)
+  },
+  {
+    path: 'recipe',
+    loadChildren: () => import('./recipe/recipe.module').then(m => m.RecipePageModule)
+  },
+  {
+    path: 'recipes',
+    loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesPageModule)
+  },
+
+  {
+    path: 'recipes',
+    redirectTo: 'recipes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tabs',
+    redirectTo: 'tabs',
+    pathMatch: 'full'
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListPageModule)
   },
 ];
 
---
+++ src/app/tabs/tabs.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+
+// import { ShoppingListPage } from '../shopping-list/shopping-list.page';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss'],
+})
+export class TabsPage implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+
+}
